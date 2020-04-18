5311924a58f06028a4ad7f6ace7088275637b2dd
Guilherme Carvalho <Guilhermbrsp@gmail.com>
2020-04-18 17:43:36 -0300

    Add solution code for stage 1

--- src/main/kotlin/com/jetacademy/simplesearchengine/Main.kt
+++ src/main/kotlin/com/jetacademy/simplesearchengine/Main.kt
@@ -1,5 +1,8 @@
 package com.jetacademy.simplesearchengine
 
 fun main() {
-
+    val wordsList = readLine()!!.split(" ")
+    val searchKeyWord = readLine()!!
+    val index = wordsList.indexOf(searchKeyWord) + 1
+    print(if (index == 0) "Not found" else index)
 }
