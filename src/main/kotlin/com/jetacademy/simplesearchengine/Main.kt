e5a10e1242353ad25a6d94c7d42ab11b9436a130
Guilherme Carvalho <Guilhermbrsp@gmail.com>
2020-04-19 10:22:47 -0300

    Add solution code for stage 3

--- src/main/kotlin/com/jetacademy/simplesearchengine/Main.kt
+++ src/main/kotlin/com/jetacademy/simplesearchengine/Main.kt
@@ -1,40 +1,58 @@
 package com.jetacademy.simplesearchengine
 
-
 fun main() {
     println("Enter the number of people:")
     val numberOfPeople = readLine()!!.toInt()
-    val peopleList: MutableList<List<String>> = mutableListOf()
+    val peopleList: MutableList<String> = mutableListOf()
     println("Enter all people:")
     repeat(numberOfPeople) {
-        peopleList.add(readLine()!!.split(" "))
+        peopleList.add(readLine()!!)
     }
-    println("Enter the number of search queries:")
-    val numberOfQueries = readLine()!!.toInt()
-    repeat(numberOfQueries) {
-        println("Enter data to search people:")
-        val searchData = readLine()!!
-        val queryResult = queryPerson(peopleList, searchData)
-        printQueryResult(queryResult)
+
+    var menuComand = -1
+    while (menuComand != 0) {
+        printMenu()
+        menuComand = readLine()!!.toInt()
+        when (menuComand) {
+            1 -> findAPerson(peopleList)
+            2 -> printAllPeople(peopleList)
+            0 -> exit()
+            else -> println("Incorrect option! Try again.")
+        }
     }
 }
 
-private fun printQueryResult(queryResult: List<List<String>>) {
+
+private fun printMenu() =
+    println(
+        "=== Menu ===\n" +
+                "1. Find a person\n" +
+                "2. Print all people\n" +
+                "0. Exit"
+    )
+
+private fun exit() = println("Bye!")
+
+private fun findAPerson(peopleList: MutableList<String>) {
+    println("Enter data to search people:")
+    val searchData = readLine()!!
+    val queryResult = queryPerson(peopleList, searchData)
+    printQueryResult(queryResult)
+}
+
+private fun printAllPeople(peopleList: MutableList<String>) {
+    println("=== List of people ===")
+    printQueryResult(peopleList)
+}
+
+private fun printQueryResult(queryResult: List<String>) {
     if (queryResult.isNotEmpty()) {
-        println("Found people:")
-        queryResult.map {
-            println("${it.getOrElse(0) { "" }} " +
-                    "${it.getOrElse(1) { "" }} " +
-                    "${it.getOrElse(2) { "" }}")
-        }
+        queryResult.map(::println)
     } else
         println("No matching people found.")
 }
 
-private fun queryPerson(peopleList: MutableList<List<String>>, queryKeyWord: String): List<List<String>> =
+private fun queryPerson(peopleList: MutableList<String>, queryKeyWord: String): List<String> =
     peopleList.filter { person ->
-        person.any { element ->
-            element.contains(queryKeyWord, ignoreCase = true)
-        }
+        person.contains(queryKeyWord, ignoreCase = true)
     }
-
