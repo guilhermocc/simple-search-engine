b8a77ace6950bbbe16f65afabf703bd74b59e4e5
Guilherme Carvalho <Guilhermbrsp@gmail.com>
2020-04-19 10:20:32 -0300

    Add solution code for stage 2

--- src/main/kotlin/com/jetacademy/simplesearchengine/Main.kt
+++ src/main/kotlin/com/jetacademy/simplesearchengine/Main.kt
@@ -1,8 +1,40 @@
 package com.jetacademy.simplesearchengine
 
+
 fun main() {
-    val wordsList = readLine()!!.split(" ")
-    val searchKeyWord = readLine()!!
-    val index = wordsList.indexOf(searchKeyWord) + 1
-    print(if (index == 0) "Not found" else index)
+    println("Enter the number of people:")
+    val numberOfPeople = readLine()!!.toInt()
+    val peopleList: MutableList<List<String>> = mutableListOf()
+    println("Enter all people:")
+    repeat(numberOfPeople) {
+        peopleList.add(readLine()!!.split(" "))
+    }
+    println("Enter the number of search queries:")
+    val numberOfQueries = readLine()!!.toInt()
+    repeat(numberOfQueries) {
+        println("Enter data to search people:")
+        val searchData = readLine()!!
+        val queryResult = queryPerson(peopleList, searchData)
+        printQueryResult(queryResult)
+    }
 }
+
+private fun printQueryResult(queryResult: List<List<String>>) {
+    if (queryResult.isNotEmpty()) {
+        println("Found people:")
+        queryResult.map {
+            println("${it.getOrElse(0) { "" }} " +
+                    "${it.getOrElse(1) { "" }} " +
+                    "${it.getOrElse(2) { "" }}")
+        }
+    } else
+        println("No matching people found.")
+}
+
+private fun queryPerson(peopleList: MutableList<List<String>>, queryKeyWord: String): List<List<String>> =
+    peopleList.filter { person ->
+        person.any { element ->
+            element.contains(queryKeyWord, ignoreCase = true)
+        }
+    }
+
